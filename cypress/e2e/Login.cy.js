586bf1aab87f2a1b2b63bb50413aef382d554b57
Eduardo Berg <duhberg@gmail.com>
2024-09-26 23:04:38 -0300

    Testes de login com diferentes usuários
    Adicionado comando personalizado para realizar login, criado teste com usuário locked_out_user

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,7 +1,15 @@
 /*
 Dado que acesso a página de login
-Quando digito usuario e senha e clica em login
+Quando digito usuário e senha do perfil standard_user e clico em login
 Então vejo a tela iniciar home logado
+====================================================================================
+Dado que acesso a página de login
+Quando digito usuário e senha incorreta e clico em login
+Então recebo mensagem de erro
+====================================================================================
+Dado que acesso a página de login
+Quando digito usuário e senha do perfil locked_out_user e clico em login
+Então recebo mensagem de erro
 */
 
 
@@ -11,11 +19,9 @@ describe('Login', () => {
       cy.visit('https://www.saucedemo.com')
     })
     
-    context('Quando digito usuário e senha e clico em login', () => {
+    context('Quando digito usuário e senha do perfil standard_user e clico em login', () => {
       beforeEach(() => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
+        cy.realizarLogin('standard_user', 'secret_sauce')
       })
         
         it('Então vejo a tela iniciar home logado', () => {
@@ -31,9 +37,18 @@ describe('Login', () => {
       })
 
         it('Então recebo mensagem de erro', () => {
-          cy.get('[data-test="error"]').should('be.visible')
+          cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
         })
     })
 
+    context('Quando digito usuário e senha do perfil locked_out_user e clico em login', () => {
+      beforeEach(() => {
+        cy.realizarLogin('locked_out_user', 'secret_sauce')
+      })
+        
+        it('Então recebo mensagem de erro', () => {
+          cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
+        })
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('realizarLogin', (usuario, senha) => {
+    cy.get('[data-test="username"]').type(usuario)
+    cy.get('[data-test="password"]').type(senha)
+    cy.get('[data-test="login-button"]').click()
+})
